8a1415886b77d2b6e9efbebcf2b8ec8ff56eee45
Bartosz Jakubowski <bjakubowski.dev@gmail.com>
2019-10-16 05:38:41 +0200

    Add session forwarding
    Yeah, at least I'm not back warding

---
+++ routes/routesUnsigned.js
@@ -0,0 +1,30 @@
+//File for managing routes and redirects
+
+module.exports = (function() {
+    let routes = require('express').Router();
+
+    routes.get('/', function(req, res){
+        res.set('Content-Type', 'text/html');
+        res.render('main.pug')
+    });
+
+    routes.get('/login', function(req, res){
+        res.set('Content-Type', 'text/html');
+        res.render('login.pug');
+    });
+
+    routes.get('*', function(req, res){
+        res.status(404).send('Error 404: Page not found')
+    });
+
+    routes.post('/login', function(req, res){
+        res.send(`Following credintial, login:  ${req.body.loginEmail}, password: ${req.body.loginPassword}`)
+        console.log(`Following credintial, login: ${req.body.loginEmail}, password: ${req.body.loginPassword}`)
+    })
+
+    routes.post('/register', function(req, res){
+        console.log(`${req.body.email} $`)
+    })
+
+    return routes;
+})();
